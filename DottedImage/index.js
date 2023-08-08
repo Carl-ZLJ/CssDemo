a4a33b4e378f53fa483ac45ed418636cb507c589
kaelzhu <zljmms32@gmail.com>
2023-08-08 14:33:06 +0800

    refactor(dotted image): oop

--- DottedImage/index.js
+++ DottedImage/index.js
@@ -1,122 +1,159 @@
-const SIZE = 420
-const MAX_R = Math.sqrt(SIZE * SIZE * 2 / 4)
+class DottedImage {
+    constructor(size = 420,) {
+        this.SIZE = size
+        this.MAX_RADIUS = Math.sqrt(this.SIZE * this.SIZE * 2 / 4)
 
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+        this.image = new Image()
+        this.fileReader = new FileReader()
+        this.oscImage = new OffscreenImage(this.SIZE)
 
-const osc = new OffscreenCanvas(SIZE, SIZE)
-const oscctx = osc.getContext('2d')
+        this.data = null
 
-const image = new Image()
-const fileReader = new FileReader()
-
-let data
-let r = 0
-let angle = 0
-
-let rOffset = 1
-let aOffset = 5
-let contrast = 1
-
-
-function compressedImage(image, size) {
-    const w = image.width
-    const h = image.height
-
-    const ratio = Math.min(w, h) / size
-    const newW = w / ratio
-    const newH = h / ratio
-    const newX = (size - newW) / 2
-    const newY = (size - newH) / 2
-
-    return {
-        newW,
-        newH,
-        newX,
-        newY,
-    }
-}
-
-function imageToData(ctx, image, size) {
-    const { newW, newH, newX, newY } = compressedImage(image, size)
-    ctx.drawImage(image, newX, newY, newW, newH)
-    const data = ctx.getImageData(0, 0, size, size).data
-    return data
-
-}
-
-function init() {
-    canvas.width = SIZE
-    canvas.height = SIZE
-    ctx.fillStyle = '#fff'
-
-    r = 0
-    angle = 0
-
-    rOffset = Number(document.querySelector('#linearOffset').value)
-    aOffset = Number(document.querySelector('#radialOffset').value) * rOffset
-
-    const tc = Number(document.querySelector('#contrast').value)
-    contrast = tc < 0 ? ((tc + 3) / 3) : (tc + 1)
-}
-
-window.onload = () => {
-    document.querySelector('#linearOffset').value = Math.round() * 1.5 + 0.5
-    document.querySelector('#radialOffset').value = Math.round() * 19 + 1
-
-    init()
-
-    image.onload = () => {
-
-        data = imageToData(oscctx, image, SIZE)
-        render()
+        this.setRandomInputValue()
+        this.setDefaultImage()
+        this.init()
+        this.bindEvents()
     }
 
-    image.crossOrigin = 'anonymous'
-    image.src = 'https://assets.codepen.io/1948355/logo.jpg'
+    init() {
+        this.initCanvas()
 
-    document.querySelector('#generate').addEventListener('click', () => {
-        init()
-        render()
-    })
+        this.initDrawingParams()
+    }
 
-    const fileInput = document.querySelector('#fileInput')
-    fileInput.addEventListener('change', () => {
-        document.querySelector('.fileInput').innerHTML = fileInput.files[0].name
+    initDrawingParams() {
+        this.r = 0
+        this.angle = 0
+        this.radiusOffset = Number(document.querySelector('#linearOffset').value)
+        this.angleOffset = Number(document.querySelector('#radialOffset').value) * this.radiusOffset
 
-        fileReader.onload = () => {
-            image.src = fileReader.result
+        const tc = Number(document.querySelector('#contrast').value)
+        this.contrast = tc < 0 ? ((tc + 3) / 3) : (tc + 1)
+    }
+
+    initCanvas() {
+        this.canvas = document.querySelector('canvas')
+        this.ctx = this.canvas.getContext('2d')
+
+        this.canvas.width = this.SIZE
+        this.canvas.height = this.SIZE
+        this.ctx.fillStyle = '#fff'
+    }
+
+    render() {
+        let count = 1000
+        let { SIZE, MAX_RADIUS, ctx, data, contrast, radiusOffset, angleOffset, } = this
+
+        while (count > 0) {
+            const x = SIZE / 2 + this.r * Math.cos(this.angle)
+            const y = SIZE / 2 + this.r * Math.sin(this.angle)
+
+            const p = (Math.round(y) * SIZE + Math.round(x)) * 4
+
+            // transform image data
+            const ap = (data[p] + data[p + 1] + data[p + 2]) / 3
+
+            const tr = Math.pow(ap / 255, contrast) * Math.PI * radiusOffset * contrast
+
+            ctx.beginPath()
+            ctx.arc(x, y, tr, 0, Math.PI * 2)
+            ctx.fill()
+
+            const thisAngle = Math.atan(angleOffset / this.r)
+            this.angle += thisAngle
+            this.r += radiusOffset * thisAngle
+
+            count--
+
+            if (this.r <= MAX_RADIUS) {
+                requestAnimationFrame(this.render.bind(this))
+            }
         }
-        fileReader.readAsDataURL(fileInput.files[0])
-    })
-}
 
-function render() {
-    let count = 1000
+    }
 
-    while (count > 0) {
-        const x = SIZE / 2 + r * Math.cos(angle)
-        const y = SIZE / 2 + r * Math.sin(angle)
+    setRandomInputValue() {
+        document.querySelector('#linearOffset').value = Math.random() * 1.5 + 0.5
+        document.querySelector('#radialOffset').value = Math.random() * 19 + 1
+    }
 
-        const p = (Math.round(y) * SIZE + Math.round(x)) * 4
+    setDefaultImage() {
+        this.image.crossOrigin = 'anonymous'
+        this.image.src = 'https://assets.codepen.io/1948355/logo.jpg'
+    }
 
-        // transform image data
-        const ap = (data[p] + data[p + 1] + data[p + 2]) / 3
+    bindEvents() {
+        this.onLoadImage()
 
-        const tr = Math.pow(ap / 255, contrast) * Math.PI * rOffset * contrast
+        this.onLoadFile()
 
-        ctx.beginPath()
-        ctx.arc(x, y, tr, 0, Math.PI * 2)
-        ctx.fill()
+        this.generateOnClick()
+    }
 
-        const thisAngle = Math.atan(aOffset / r)
-        angle += thisAngle
-        r += rOffset * thisAngle
+    generateOnClick() {
+        document.querySelector('#generate').addEventListener('click', () => {
+            this.init()
+            this.render()
+        })
+    }
 
-        count--
-
-        if (r <= MAX_R) {
-            requestAnimationFrame(render)
+    onLoadImage() {
+        this.image.onload = () => {
+            this.data = this.oscImage.imageToData(this.image, this.SIZE)
+            this.render()
         }
     }
+
+    onLoadFile() {
+        const fileInput = document.querySelector('#fileInput')
+        fileInput.addEventListener('change', () => {
+            document.querySelector('.fileInput').innerHTML = fileInput.files[0].name
+
+            this.fileReader.onload = () => {
+                this.image.src = this.fileReader.result
+            }
+            this.fileReader.readAsDataURL(fileInput.files[0])
+        })
+    }
 }
+
+class OffscreenImage {
+    constructor(size) {
+        this.canvas = new OffscreenCanvas(size, size)
+        this.ctx = this.canvas.getContext('2d', { willReadFrequently: true })
+    }
+
+    imageToData(image, size) {
+        const { newW, newH, newX, newY } = this.newImageLoc(image, size)
+        this.ctx.drawImage(image, newX, newY, newW, newH)
+        const data = this.ctx.getImageData(0, 0, size, size).data
+        return data
+
+    }
+
+    newImageLoc(image, size) {
+        const w = image.width
+        const h = image.height
+
+        const ratio = Math.min(w, h) / size
+        const newW = w / ratio
+        const newH = h / ratio
+        const newX = (size - newW) / 2
+        const newY = (size - newH) / 2
+
+        return {
+            newW,
+            newH,
+            newX,
+            newY,
+        }
+    }
+}
+
+
+
+function __main() {
+    new DottedImage()
+}
+
+__main()
